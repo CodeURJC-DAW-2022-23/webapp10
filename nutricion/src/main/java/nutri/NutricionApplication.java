b14cf424b17190333b3d6cc0d1f8d2457fcfa593
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-01-30 21:42:09 +0100

    Prueba

---
+++ nutricion/src/main/java/nutri/NutricionApplication.java
@@ -0,0 +1,13 @@
+package nutri.web.nutricion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricionApplication.class, args);
+	}
+
+}
