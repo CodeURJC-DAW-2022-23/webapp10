08bd03a37d69adf0bd508e912894dcbbcd2f2f70
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-01-31 13:35:50 +0100

    Front pages update. Login and presets

--- nutricion/src/main/java/nutri/NutricionApplication.java
+++ nutricion/src/main/java/nutri/NutricionApplication.java
@@ -1,4 +1,4 @@
-package nutri;
+package nutri.web.nutricion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ nutricion/src/main/resources/static/js/login.js
@@ -0,0 +1,10 @@
+language="JavaScript" charset="UTF-8">
+        function mostrar() {
+            div = document.getElementById('login');
+            div.style.display ='';
+        }
+
+        function cerrar() {
+            div = document.getElementById('login');
+            div.style.display = 'none';
+        }
