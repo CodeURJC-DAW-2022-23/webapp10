9887b1f99886724feba09a2920c4ff60de70e341
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-02-05 12:41:30 +0100

    AdminCharts Template Update

--- nutricion/src/main/resources/static/js/demo/chart-pie-demo.js
+++ nutricion/src/main/resources/static/js/demo/chart-pie-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Cut", "Bulk", "Smart"],
     datasets: [{
-      data: [55, 30, 15],
+      data: [33, 33, 33],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
