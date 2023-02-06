7d8c9ddff8b0df0f65123946793cd55eab0b035c
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-02-06 17:43:43 +0100

    UserRepository

--- nutricion/src/main/java/nutri/Controller/AdminController.java
+++ nutricion/src/main/java/nutri/Controller/AdminController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
 
 
+
     @GetMapping("/admin")
     public String showAdmin(){
         return "USR_Admin";
@@ -21,5 +22,7 @@ public class AdminController {
     }
     @GetMapping("/profileInfo")
     public String showProfile(){return "USR_ProfileInfoAdmin";}
+
+
 
 }
