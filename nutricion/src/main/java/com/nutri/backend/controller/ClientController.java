f3795bd3fc5feff2b93faae3b0b182491bf6f8ee
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-02-13 16:16:52 +0100

    Form insertion in User Model

--- nutricion/src/main/java/com/nutri/backend/controller/ClientController.java
+++ nutricion/src/main/java/com/nutri/backend/controller/ClientController.java
@@ -1,39 +1,85 @@
 package com.nutri.backend.controller;
 
+import com.nutri.backend.model.Form;
+import com.nutri.backend.model.User;
+import com.nutri.backend.repositories.FormRepository;
+import com.nutri.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class ClientController {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private FormRepository formRep;
+
 	@GetMapping("/clientDiets")
-	public String diet() {
+	public String diet(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ClientDiets";
 	}
 
 	@GetMapping("/clientForm")
-	public String forms() {
+	public String forms(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByName(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ClientForm";
 	}
 	@GetMapping("/clientRecipes")
-	public String recipes() {
+	public String recipes(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ClientRecepies";
 	}
 
 	@GetMapping("/clientChart")
-	public String chart() {
+	public String chart(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ClientCharts";
 	}
 
 	@GetMapping("/clientInfo")
-	public String showInfo() {
+	public String showInfo(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ProfileInfoClient";
 	}
 
 	@GetMapping("/clientInfoSetting")
-	public String editInfo() {
+	public String editInfo(Model model, HttpServletRequest request) {
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		model.addAttribute("name", user.getName());
 		return "USR_ProfileClientEdit";
 	}
+
+
+
+	@GetMapping("/clientFormUpdate")
+	public String clientForm(Model model, HttpServletRequest request,@RequestParam String gensex, @RequestParam String age,@RequestParam String phactivity,
+							 @RequestParam int weight,@RequestParam int height, @RequestParam String interest,
+							 @RequestParam String aspiration){
+		String name = request.getUserPrincipal().getName();
+		User user = userRepository.findByEmail(name).orElseThrow();
+		Form newForm = new Form(gensex,age,phactivity,weight,height,interest,aspiration);
+		formRep.save(newForm);
+		model.addAttribute("name", user.getName());
+		return "redirect:/ClientDiets";
+	}
+
 }
