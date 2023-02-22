6542efeb1dfdb6d0b21226b1a5c37297c8f695d8
Your Name <you@example.com>
2023-02-22 17:23:05 +0100

    image database

---
+++ nutricion/src/main/java/com/nutri/backend/repositories/ImageRepository.java
@@ -0,0 +1,13 @@
+package com.nutri.backend.repositories;
+
+import com.nutri.backend.model.Form;
+import com.nutri.backend.model.Image;
+import com.nutri.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image,Long> {
+    Optional<Image> findByName(String name);
+    Optional<Image> findByEmail(String email);
+}
