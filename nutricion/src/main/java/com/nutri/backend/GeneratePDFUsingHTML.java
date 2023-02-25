6c159fd9be76dc095a655901b9433afedcf7cff9
Sara G <saraglezt8@gmail.com>
2023-02-25 12:01:43 +0100

    PDF v1

---
+++ nutricion/src/main/java/com/nutri/backend/GeneratePDFUsingHTML.java
@@ -0,0 +1,15 @@
+package com.nutri.backend;
+
+import com.itextpdf.html2pdf.HtmlConverter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GeneratePDFUsingHTML {
+
+    public static void GeneratePDFUsingHTML() throws IOException {
+
+        HtmlConverter.convertToPdf(new File("./pdf-input.html"),new File("demo-html.pdf"));
+    }
+}
+
---
+++ nutricion/src/main/java/com/nutri/backend/controller/PDFController.java
@@ -0,0 +1,102 @@
+package com.nutri.backend.controller;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.nutri.backend.model.Recepy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import com.nutri.backend.repositories.RecepyRepository;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/recipePDF")
+public class PDFController {
+
+
+    @Autowired
+    ServletContext servletContext;
+
+    @Autowired
+    private RecepyRepository recepyRepository;
+
+    private final TemplateEngine templateEngine;
+
+    public PDFController(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    @PostMapping("/")
+    public String getRecipePage(Model model, @RequestParam(required = false) List<Long> id) {
+        if(id != null) {
+            for (Long l : id) {
+                Optional<Recepy> recipe = recepyRepository.findById(l);
+                model.addAttribute("recipeEntry", recipe);
+            }
+        }else{
+            return "redirect:/viewRecipe";
+        }
+        return "recipe";
+    }
+
+    @PostMapping("/downloadRecepy")
+    public Object getPDF(Model model, HttpServletRequest request, HttpServletResponse response, @RequestParam(required = false) List<Long> id) throws IOException {
+
+        /* Do Business Logic*/
+
+        if(id != null) {
+            //Recepy[] recipes = new Recepy[id.size()];
+            int i = 0;
+            Optional<Recepy> recipe = null;
+            for (Long l : id) {
+                recipe = recepyRepository.findById(l);
+                //recipes = recipe;
+                i++;
+            }
+            /* Create HTML using Thymeleaf template Engine */
+
+            WebContext context = new WebContext(request, response, servletContext);
+            context.setVariable("recipe", recipe);
+            //model.addAttribute("description", recipe.getDescription());
+            String recipeHtml = templateEngine.process("USR_ClientRecipePDF", context);
+
+            /* Setup Source and target I/O streams */
+
+            ByteArrayOutputStream target = new ByteArrayOutputStream();
+            ConverterProperties converterProperties = new ConverterProperties();
+            converterProperties.setBaseUri("http://localhost:8080");
+            /* Call convert method */
+            HtmlConverter.convertToPdf(recipeHtml, target, converterProperties);
+
+            /* extract output as bytes */
+            byte[] bytes = target.toByteArray();
+
+
+            /* Send the response as downloadable PDF */
+
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=recipe.pdf")
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(bytes);
+
+        }
+        return "redirect:/viewRecipe";
+    }
+
+}
