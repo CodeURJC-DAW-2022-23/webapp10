9ffe5e56d6100e516650552b22ba7a20d169a80a
Sara G <saraglezt8@gmail.com>
2023-02-27 17:03:48 +0100

    PDFController updated (no diets)

--- nutricion/src/main/java/com/nutri/backend/controller/PDFController.java
+++ nutricion/src/main/java/com/nutri/backend/controller/PDFController.java
@@ -2,16 +2,13 @@ package com.nutri.backend.controller;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
-import com.nutri.backend.model.Diet;
 import com.nutri.backend.model.Recepy;
-import com.nutri.backend.repositories.DietRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,9 +36,6 @@ public class PDFController {
     @Autowired
     private RecepyRepository recepyRepository;
 
-    @Autowired
-    private DietRepository dietRepository;
-
     private final TemplateEngine templateEngine;
 
     public PDFController(TemplateEngine templateEngine) {
@@ -68,12 +62,12 @@ public class PDFController {
 
         if(id != null) {
             //Recepy[] recipes = new Recepy[id.size()];
-            int i = 0;
+            //int i = 0;
             Optional<Recepy> recipe = null;
             for (Long l : id) {
                 recipe = recepyRepository.findById(l);
                 //recipes[i] = recepyRepository.findById(l).get();
-                i++;
+                //i++;
             }
             /* Create HTML using Thymeleaf template Engine */
 
@@ -110,48 +104,6 @@ public class PDFController {
 
         }
         return "redirect:/viewRecipe";
-    }
-
-    @PostMapping("/downloadDiet")
-    public Object getDietPDF(Model model, HttpServletRequest request, HttpServletResponse response, @RequestParam(required = false) List<Long> id) throws IOException {
-
-        /* Do Business Logic*/
-        System.out.println(id);
-        if(id != null) {
-            int i = 0;
-            Optional<Diet> diet = null;
-            for (Long l : id) {
-                diet = dietRepository.findById(l);
-                i++;
-            }
-            /* Create HTML using Thymeleaf template Engine */
-
-            WebContext context = new WebContext(request, response, servletContext);
-            context.setVariable("diet", diet);
-            //model.addAttribute("description", recipe.getDescription());
-            String dietHtml = templateEngine.process("USR_ClientDietPDF", context);
-
-            /* Setup Source and target I/O streams */
-
-            ByteArrayOutputStream target = new ByteArrayOutputStream();
-            ConverterProperties converterProperties = new ConverterProperties();
-            converterProperties.setBaseUri("http://localhost:8080");
-            /* Call convert method */
-            HtmlConverter.convertToPdf(dietHtml, target, converterProperties);
-
-            /* extract output as bytes */
-            byte[] bytes = target.toByteArray();
-
-
-            /* Send the response as downloadable PDF */
-
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=diet.pdf")
-                    .contentType(MediaType.APPLICATION_PDF)
-                    .body(bytes);
-
-        }
-        return "redirect:/viewDiet";
     }
 
 }
