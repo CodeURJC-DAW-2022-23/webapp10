8de2658dc744671f24d9876e7df08241f0be7a71
Your Name <you@example.com>
2023-04-01 18:40:39 +0200

    login module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,8 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
+    LoginComponent
 
   ],
   imports: [
@@ -12,6 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [
+    LoginComponent
+  ]
 })
 export class AppModule { }
---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Output, ResolvedReflectiveFactory } from "@angular/core";
+//import { User } from "src/app/models/User.model";
+//import { LoginService } from "src/app/services/login.service";
+
+
+@Component({
+    selector:'app-root',
+    templateUrl: './login.component.html',
+    styleUrls: ['../../../assets/css/main.css']
+})
+export class LoginComponent {
+  user='adri'
+}
