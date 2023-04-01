e0e0e0e59e2dfe09860da1b013fe98730f58a53b
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-04-01 17:08:15 +0200

    bootstrap

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,FormsModule]
 })
 export class AppRoutingModule { }
