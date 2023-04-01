0cc04979fc98d727d17712d045e26fa024a5ef4b
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-04-01 18:13:34 +0200

    styles commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {AppComponent} from './components/mainPage/mainPage.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainHeader } from './components/headerMainPage/header.component';
 
 @NgModule({
   declarations: [
-
+    AppComponent,
+    MainHeader
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
