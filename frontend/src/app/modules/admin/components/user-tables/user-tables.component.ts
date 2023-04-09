7a2218ce02df25304ffc5df81b03827090162e1a
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-04-09 20:34:28 +0200

    user-table and client-table introduction

--- frontend/src/app/modules/admin/components/user-tables/user-tables.component.ts
+++ frontend/src/app/modules/admin/components/user-tables/user-tables.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-tables',
   templateUrl: './user-tables.component.html',
-  styleUrls: ['./user-tables.component.css']
+  styleUrls: ['../stats/stats.component.css','./user-tables.component.css']
 })
 export class UserTablesComponent {
 
