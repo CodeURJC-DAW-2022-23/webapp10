6f257c8448d60f090af4029751cfad249310349a
Sara G <saraglezt8@gmail.com>
2023-04-09 20:23:30 +0200

    Client components (dashboard, form, diet and recipes) - unfinished

---
+++ frontend/src/app/modules/client/client.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientRoutingModule } from './client-routing.module';
+import { ClientDashboardComponent } from './components/client-dashboard/client-dashboard.component';
+import { FormComponent } from './components/form/form.component';
+import { DietComponent } from './components/diet/diet.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    ClientDashboardComponent,
+    FormComponent,
+    DietComponent,
+    RecipesComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    ClientRoutingModule
+  ]
+})
+export class ClientModule { }
