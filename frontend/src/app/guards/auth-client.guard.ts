ba570feaab593b74704802a9cbce5371544226b9
Your Name <you@example.com>
2023-04-14 17:53:47 +0200

    guard client

---
+++ frontend/src/app/guards/auth-client.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, map, of } from 'rxjs';
+import { User } from '../models/User.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthClientGuard implements CanActivate {
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):  Observable<boolean> | Promise<boolean> | boolean {
+    return this.http.get('/api/users/me').pipe(
+      map((user: User) => {
+        if (user.userType==='client') {
+          return true;
+        } else {
+          this.router.navigate(['login']);
+          return false;
+        }
+      }),
+      catchError((error: any) => {
+        this.router.navigate(['login']);
+        return of(false);
+
+      })
+    ) as Observable<boolean>;
+  }
+
+}
