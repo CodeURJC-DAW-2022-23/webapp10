f439fb8b9ab1c8ec120d10844fc1a967be8b8062
Your Name <you@example.com>
2023-04-15 13:33:05 +0200

    header worker logout

--- frontend/src/app/modules/worker/components/header/header.component.ts
+++ frontend/src/app/modules/worker/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/User.model';
+import { UserService } from 'src/app/services/User.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  showMenu:boolean= false;
+  user:User | undefined;
+  constructor(private loginService: LoginService,private router:Router,private userSevice: UserService,){this.user=this.loginService.currentUser()}
 
+  show(){
+    this.showMenu=!this.showMenu;
+  }
+
+  logout(){
+    this.loginService.logOut().subscribe(
+      _=>this.userSevice.getMe().subscribe((data:any)=>console.log(data)
+      )
+
+    )
+
+    this.router.navigate([''])
+  }
 }
