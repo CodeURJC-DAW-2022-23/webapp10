67a24c7c75a38dab9252b64d3fcb92e1b598bcf1
steyex <bysteyex@gmail.com>
2023-04-17 23:28:21 +0200

    imagen

--- frontend/src/app/modules/worker/components/settings/settings.component.ts
+++ frontend/src/app/modules/worker/components/settings/settings.component.ts
@@ -11,18 +11,27 @@ import { catchError } from 'rxjs/operators';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
-  user:User={
-    name:'',
-    surname:'',
-    email:'',
-    encodedPassword:''
-  }
+  user: User = {
+    name: '',
+    surname: '',
+    email: '',
+    encodedPassword: '',
+    image: undefined
+  };
 
   constructor(private router: Router, private httpClient: HttpClient, private userService: UserService) {
     userService.getMe().subscribe(
       user => this.user = user as User,
       error => alert("No fue posible cargar sus datos del servidor. Inténtelo más tarde.")
     )};
+    
+    onImageSelected(event: Event) {
+      const file = (event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files![0];
+      if (file) {
+        this.user.image = file;
+      }
+    }
+    
   
     save() {
       this.userService.updateProfile(this.user as User).subscribe(
