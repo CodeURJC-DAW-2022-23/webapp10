a0d4450c822342c01f61dea28e002b8642c5ca47
ADRI\Adrian <adrianpenaga@gmail.com>
2023-04-18 10:04:46 +0200

    component diet new

--- frontend/src/app/modules/worker/components/upload-diet/upload-diet.component.ts
+++ frontend/src/app/modules/worker/components/upload-diet/upload-diet.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Diet } from 'src/app/models/Diet.model';
+import { Recepie } from 'src/app/models/Recepie.model';
 import { Triplet } from 'src/app/models/Triplet.model';
 import { UserService } from 'src/app/services/User.service';
 
@@ -13,8 +14,26 @@ import { UserService } from 'src/app/services/User.service';
 export class UploadDietComponent {
     diet : Diet|undefined;
     week : Triplet []|undefined;
+    breakfast : Recepie []|undefined;
+    lunch : Recepie []|undefined;
+    dinner : Recepie []|undefined;
+    mon:Triplet;
+    tus:Triplet;
+    thr:Triplet;
+    wen:Triplet;
+    fri:Triplet;
+    sat:Triplet;
+    sun:Triplet;
     constructor(private router: Router,private httpClient: HttpClient,private UserService :UserService){
-
+      UserService.getAllRecipesByType('Breakfast').subscribe(
+        breakfast=>this.breakfast = breakfast as Recepie[]
+      )
+      UserService.getAllRecipesByType('Lunch').subscribe(
+        lunch=>this.lunch = lunch as Recepie[]
+      )
+      UserService.getAllRecipesByType('Dinner').subscribe(
+        dinner=>this.dinner = dinner as Recepie[]
+      )
     }
     pushDiet(diet: Diet){
       this.UserService.postDiet(this.diet as Diet).subscribe(
@@ -24,10 +43,8 @@ export class UploadDietComponent {
       );
     }
 
-    uploadDiet(mb:String,ml:String,md:String,tb:String,tl:String,td:String
-      ,wb:String,wl:String,wd:String,thb:String,thl:String,thd:String,
-      fb:String,fl:String,fd:String,stb:String,stl:String,std:String,
-      snb:String,snl:String,snd:String){
+    uploadDiet(){
+
     }
 }
 
