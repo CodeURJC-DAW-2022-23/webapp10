b898d4477734da544f34048760261db4880d2ad3
steyex <bysteyex@gmail.com>
2023-04-20 12:02:47 +0200

    Intentos de imagen, diets client part1

--- frontend/src/app/modules/client/components/diet/diet.component.ts
+++ frontend/src/app/modules/client/components/diet/diet.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/User.service';
+import { Diet } from 'src/app/models/Diet.model';
 
 @Component({
   selector: 'app-diet',
   templateUrl: './diet.component.html',
   styleUrls: ['./diet.component.css']
 })
-export class DietComponent {
+export class DietComponent implements OnInit {
+
+  diets: Diet[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getDiets().subscribe(
+      diets => this.diets = diets as Diet[],
+      error => alert("No fue posible cargar las dietas del servidor. Inténtelo más tarde.")
+    );
+  }
 
 }
+
--- frontend/src/app/modules/worker/components/settings/settings.component.ts
+++ frontend/src/app/modules/worker/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { User } from 'src/app/models/User.model';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -17,30 +17,41 @@ export class SettingsComponent {
     email: '',
     encodedPassword: '',
   };
-  image: Blob | undefined = undefined;
+  @ViewChild("file")
+    file: any;
 
-  constructor(private router: Router, private httpClient: HttpClient, private userService: UserService) {
-    userService.getMe().subscribe(
-      user => this.user = user as User,
-      error => alert("No fue posible cargar sus datos del servidor. Inténtelo más tarde.")
-    )};
-    
+    constructor(private router: Router, private httpClient: HttpClient, private userService: UserService) {
+        this.userService.getMe().subscribe(
+          user => this.user = user,
+        )
+    };
+
     onImageSelected(event: Event) {
-      const file = (event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files![0];
-      if (file) {
-        this.image = file;
-      }
+        const file = (event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files![0];
+        if (file) {
+            this.file = file;
+        }
     }
-    
-  
+
     save() {
-      this.userService.updateProfile(this.user as User).subscribe(
-        _ => this.router.navigate(['worker']),
-        error => alert("No fue posible guardar los cambios. Inténtelo más tarde.")
-      )
-      this.userService.updateImage(this.image as Blob).subscribe(
-      _=> this.router.navigate(['Worker']),
-      error => alert("No fue posible guardar la imagen. Inténtelo más tarde.")
-      )
+        this.userService.updateProfile(this.user as User).subscribe(
+            _ => {
+                this.updateImg(this.user);
+            },
+            error => alert("No fue posible guardar los cambios. Inténtelo más tarde.")
+        );
+    }
+
+    updateImg(user: User): void {
+      if (this.file) {
+        this.userService.updateImage(this.file).subscribe(
+          _ => {
+            this.router.navigate(["worker"]);
+          },
+          error => alert("Error uploading user image")
+        );
+      } else {
+        this.router.navigate(["worker"]);
+      }
     }
 }
