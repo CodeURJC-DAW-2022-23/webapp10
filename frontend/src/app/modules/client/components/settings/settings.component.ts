65d569156a87303abc0b87854b37594b8752e00a
Your Name <you@example.com>
2023-04-20 17:45:09 +0200

    trys image upload

--- frontend/src/app/modules/client/components/settings/settings.component.ts
+++ frontend/src/app/modules/client/components/settings/settings.component.ts
@@ -7,7 +7,7 @@ import { UserService } from 'src/app/services/User.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent {
   user: User = {
@@ -16,31 +16,33 @@ export class SettingsComponent {
     email: '',
     encodedPassword: '',
   };
-  image: Blob | undefined = undefined;
+  foto: Blob | undefined = undefined;
 
-  constructor(private router: Router, private httpClient: HttpClient, private userService: UserService) {
+  constructor(
+    private router: Router,
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) {
     userService.getMe().subscribe(
-      user => this.user = user as User,
-      error => alert("No fue posible cargar sus datos del servidor. Inténtelo más tarde.")
-    )};
+      (user) => (this.user = user as User),
+      (error) =>
+        alert(
+          'No fue posible cargar sus datos del servidor. Inténtelo más tarde.'
+        )
+    );
+  }
 
-    onImageSelected(event: Event) {
-      const file = (event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files![0];
-      if (file) {
-        this.image = file;
-      }
+
+
+  save() {
+    this.userService.updateProfile(this.user as User).subscribe(
+      (_) => this.router.navigate(['./client/settings']),
+      (error) =>
+        alert('No fue posible guardar los cambios. Inténtelo más tarde.')
+    );
+    this.userService.updateImage(this.foto as Blob).subscribe(
+      (_) => this.router.navigate(['./client/settings']),
+      (error) => alert('No fue posible guardar la imagen. Inténtelo más tarde.')
+    );
+  }
 }
-save() {
-  this.userService.updateProfile(this.user as User).subscribe(
-    _ => this.router.navigate(['worker']),
-    error => alert("No fue posible guardar los cambios. Inténtelo más tarde.")
-  )
-  this.userService.updateImage(this.image as Blob).subscribe(
-  _=> this.router.navigate(['Worker']),
-  error => alert("No fue posible guardar la imagen. Inténtelo más tarde.")
-  )
-}
-
-}
-
-
