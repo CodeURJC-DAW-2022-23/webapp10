4330567b3438b744c200612efb5535c26fa37a42
Sara G <saraglezt8@gmail.com>
2023-04-22 20:03:49 +0200

    client profile picture

--- frontend/src/app/modules/client/components/settings/settings.component.ts
+++ frontend/src/app/modules/client/components/settings/settings.component.ts
@@ -21,37 +21,35 @@ export class SettingsComponent {
   file: any;
 
 
-  constructor(
-    private router: Router,
-    private httpClient: HttpClient,
-    private userService: UserService
-  ) {
-    userService.getMe().subscribe(
-      (user) => (this.user = user as User),
-      (error) =>
-        alert(
-          'No fue posible cargar sus datos del servidor. Inténtelo más tarde.'
-        )
-    );
-  }
+  constructor(private router: Router, private httpClient: HttpClient, private userService: UserService) {
+    this.userService.getMe().subscribe(
+      user => this.user = user,
+    )
+};
 
-  save() {
-    this.userService.updateProfile(this.user as User).subscribe(
-      (error) =>
-        alert('No fue posible guardar los cambios. Inténtelo más tarde.')
-    );
-    window.location.reload();
-  }
-  uploadImage(){
-    const image = this.file.nativeElement.files[0]
-    if (image) {
-        let formData = new FormData();
-        formData.append("imageFile", image);
-        this.userService.updateImage(formData).subscribe(
-            (error) => alert('No fue posible guardar la imagen. Inténtelo más tarde.')
-        );
-        window.location.reload();
+onImageSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files && (event.target as HTMLInputElement).files![0];
+    if (file) {
+        this.file = file;
     }
-
+}
+save() {
+  this.userService.updateProfile(this.user as User).subscribe(
+    (error) =>
+      alert('No fue posible guardar los cambios. Inténtelo más tarde.')
+  );
+  window.location.reload();
+}
+uploadImage(){
+  const image = this.file.nativeElement.files[0]
+  if (image) {
+      let formData = new FormData();
+      formData.append("imageFile", image);
+      this.userService.updateImage(formData).subscribe(
+          (error) => alert('No fue posible guardar la imagen. Inténtelo más tarde.')
+      );
+      window.location.reload();
   }
+
+}
 }
