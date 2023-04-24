6d9dd7532f7f9ba2c64865b420b0d01cd55c7207
ruky00 <80951317+ruky00@users.noreply.github.com>
2023-04-24 17:44:21 +0200

    little changes for client diet

--- frontend/src/app/modules/client/components/recipes/recipes.component.ts
+++ frontend/src/app/modules/client/components/recipes/recipes.component.ts
@@ -13,17 +13,8 @@ export class RecipesComponent {
   constructor(private userService: UserService, private router: Router) {
     userService.getUserRecipes(0).subscribe(
       recipes => this.recipes = recipes as Recepie[],
-      error => alert("No fue posible cargar los clientes del servidor. Inténtelo más tarde.")
+      error => alert("No fue posible cargar las recetas del servidor. Inténtelo más tarde.")
     )
   }
-  selectItem(item: number){
-    this.recipes?.splice(0,this.recipes.length);
-    this.userService.getAllRecipes(item-1).subscribe(
-      recipes => {
-          for (let e of recipes as Recepie [])
-                  this.recipes?.push(e);
-      },
-      error => alert("No fue posible cargar las recetas del servidor. Inténtelo más tarde.")
-  )
-  }
+
 }
