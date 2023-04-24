b0a7c72192e8cea9c07ff939e934f219096bb9de
ADRI\Adrian <adrianpenaga@gmail.com>
2023-04-24 11:52:02 +0200

    recipes component client

--- frontend/src/app/modules/client/components/recipes/recipes.component.spec.ts
+++ frontend/src/app/modules/client/components/recipes/recipes.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Recepie } from 'src/app/models/Recepie.model';
+import { UserService } from 'src/app/services/User.service';
 
-import { RecipesComponent } from './recipes.component';
-
-describe('RecipesComponent', () => {
-  let component: RecipesComponent;
-  let fixture: ComponentFixture<RecipesComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RecipesComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RecipesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent {
+  recipes: Recepie[]|undefined;
+  constructor(private userService: UserService, private router: Router) {
+    userService.getUserRecipes(0).subscribe(
+      recipes => this.recipes = recipes as Recepie[],
+      error => alert("No fue posible cargar los clientes del servidor. Inténtelo más tarde.")
+    )
+  }
+  selectItem(item: number){
+    this.recipes?.splice(0,this.recipes.length);
+    this.userService.getAllRecipes(item-1).subscribe(
+      recipes => {
+          for (let e of recipes as Recepie [])
+                  this.recipes?.push(e);
+      },
+      error => alert("No fue posible cargar las recetas del servidor. Inténtelo más tarde.")
+  )
+  }
+}
